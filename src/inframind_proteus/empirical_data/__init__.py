bf749e2a44b188610625da58423ba8260e295ff8
paulocv <42674294+paulocv@users.noreply.github.com>
2026-06-09 16:47:18 -0300

    Standardize dengue time series for all UFs

---
+++ src/inframind_proteus/empirical_data/__init__.py
@@ -0,0 +1,4 @@
+"""Module for empirical data structures and methods.
+"""
+
+from .disease_time_series import DiseaseTimeSeriesVariables
--- src/inframind_proteus/empirical_data/disease_time_series.py
+++ src/inframind_proteus/empirical_data/disease_time_series.py
@@ -2,22 +2,38 @@
 Represent time series of epidemiological diseases data (number of cases, infections,
 hospitalizations, etc.)
 """
+from dataclasses import dataclass
 from pathlib import Path
 
 import pandas as pd
 
 #TODO: Needs to be implemented
 
-class DiseaseTimeSeries:
-    """"""
-    location_id_variable: str
-    time_variable: str
-    value_variable: str
-    disease: str
+@dataclass
+class DiseaseTimeSeriesVariables:
+    """Variable names and metadata for one disease time series.
 
-    _df: pd.DataFrame
+    Formalizes the variable contract.
+    """
+    location_id_variable: str = "geocode"
+    time_variable: str = "date"
+    value_variable: str = "value"
+    disease: str = "dengue"
 
-    @classmethod
-    def from_csv_file(cls, fpath: Path):
-        """"""
+
+# class DiseaseTimeSeries:
+#     """"""
+#
+#
+#     _df: pd.DataFrame
+#
+#     @classmethod
+#     def from_csv_file(cls, fpath: Path):
+#         """"""
+#
+#         fpath = Path(fpath)
+#         if not fpath.exists():
+#             raise FileNotFoundError(f"Disease time series file not found: {fpath}")
+#
+#         df = pd.read_csv(fpath)
 
